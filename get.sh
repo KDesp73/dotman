8490147f5be36f539d1f7abc3dc5b5b6f0a5a959
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-09-13 07:10:41 +0300

    updated get.sh

--- get.sh
+++ get.sh
@@ -4,18 +4,18 @@ delete() {
     local -n files=$1
 
     for file in "${files[@]}"; do
-        rm -r "./dotman/$file"
+        rm -rf "./dotman/$file"
     done
 }
 
 to_remove=(
-    get.sh
-    README.md
-    CHANGELOG.md
-    .gitignore
-    .git
-    dotman
+    "get.sh"
+    "README.md"
+    "CHANGELOG.md"
+    ".gitignore"
+    ".git"
 )
 
 git clone --depth=1 https://github.com/KDesp73/dotman
 delete to_remove
+rm -rf dotman
