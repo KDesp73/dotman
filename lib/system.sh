7a0de528da9eb4b6ac1e6c7a3e15470f99f5bef5
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-09-15 14:15:35 +0300

    made variables local

--- lib/system.sh
+++ lib/system.sh
@@ -6,78 +6,80 @@ source "$BASE_DIR"/lib/ansi.sh
 get_distro() {
     if [ -f /etc/os-release ]; then
         . /etc/os-release
-        DISTRO=$ID
+        distro=$ID
     elif type lsb_release >/dev/null 2>&1; then
-        DISTRO=$(lsb_release -i -s)
+        distro=$(lsb_release -i -s)
     elif [ -f /etc/debian_version ]; then
-        DISTRO=debian
+        distro=debian
     elif [ -f /etc/redhat-release ]; then
-        DISTRO=rhel
+        distro=rhel
     elif [ -f /etc/system-release ]; then
-        DISTRO=$(awk '{print $1}' /etc/system-release)
+        distro=$(awk '{print $1}' /etc/system-release)
     else
-        DISTRO=unknown
+        distro=unknown
     fi
-    echo "$DISTRO"
+    echo "$distro"
 }
 
 get_installation_command() {
-    local DISTRO=$1
-    case $DISTRO in
+    local distro=$1
+    local installation_command=""
+    case $distro in
         ubuntu|debian|linuxmint)
-            INSTALLATION_COMMAND="sudo apt-get install -y"
+            installation_command="sudo apt-get install -y"
             ;;
         fedora|centos|rhel)
-            INSTALLATION_COMMAND="sudo yum install -y"
+            installation_command="sudo yum install -y"
             ;;
         arch)
-            INSTALLATION_COMMAND="sudo pacman -S --noconfirm"
+            installation_command="sudo pacman -S --noconfirm"
             ;;
         manjaro|ManjaroLinux)
-            INSTALLATION_COMMAND="pamac install --no-confirm --no-upgrade"
+            installation_command="pamac install --no-confirm --no-upgrade"
             ;;
         gentoo)
-            INSTALLATION_COMMAND="sudo emerge --ask"
+            installation_command="sudo emerge --ask"
             ;;
         nixos)
-            INSTALLATION_COMMAND="echo \"Warning:\nUsing nix-env permanently modifies a local profile of installed packages. 
+            installation_command="echo \"Warning:\nUsing nix-env permanently modifies a local profile of installed packages. 
                                         This must be updated and maintained by the user in the same way as with a traditional package manager, 
                                         foregoing many of the benefits that make Nix uniquely powerful.\nUsing nix-shell or a NixOS configuration is recommended instead. \"; nix-env -iA"
             ;;
         *)
-            INSTALLATION_COMMAND="unknown"
+            installation_command="unknown"
             ;;
     esac
-    echo "$INSTALLATION_COMMAND"
+    echo "$installation_command"
 }
 
 get_uninstallation_command() {
-    local DISTRO=$1
-    case $DISTRO in
+    local distro=$1
+    local uninstallation_command=""
+    case $distro in
         ubuntu|debian|linuxmint)
-            UNINSTALLATION_COMMAND="sudo apt remove -y"
+            uninstallation_command="sudo apt remove -y"
             ;;
         fedora|centos|rhel)
-            UNINSTALLATION_COMMAND="sudo yum remove -y"
+            uninstallation_command="sudo yum remove -y"
             ;;
         arch)
-            UNINSTALLATION_COMMAND="sudo pacman -Rs --noconfirm"
+            uninstallation_command="sudo pacman -Rs --noconfirm"
             ;;
-        manjaro)
-            UNINSTALLATION_COMMAND="pamac remove --no-confirm"
+        manjaro|ManjaroLinux)
+            uninstallation_command="pamac remove --no-confirm"
             ;;
         gentoo)
-            UNINSTALLATION_COMMAND="sudo emerge --unmerge --ask"
+            uninstallation_command="sudo emerge --unmerge --ask"
             ;;
         nixos)
-            INSTALLATION_COMMAND="echo \"Warning:\nUsing nix-env permanently modifies a local profile of installed packages. 
+            uninstallation_command="echo \"Warning:\nUsing nix-env permanently modifies a local profile of installed packages. 
                                         This must be updated and maintained by the user in the same way as with a traditional package manager, 
                                         foregoing many of the benefits that make Nix uniquely powerful.\nUsing nix-shell or a NixOS configuration is recommended instead. \"; nix-env --uninstall"
             ;;
         *)
-            UNINSTALLATION_COMMAND="unknown"
+            uninstallation_command="unknown"
             ;;
     esac
-    echo "$UNINSTALLATION_COMMAND"
+    echo "$uninstallation_command"
 }
 
