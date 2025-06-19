0c41294554e4c2096f062faffb4e96f02943073f
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-06-19 23:18:00 +0300

    fix: freeing memory

--- src/commands/update.zig
+++ src/commands/update.zig
@@ -18,6 +18,7 @@ pub fn run(context: *ctx.Context) !void {
 
     const VERSION_URL = "https://raw.githubusercontent.com/KDesp73/dotman/refs/heads/main/docs/VERSION";
     var res = try system.runCommand(allocator, "curl -s {s}", .{VERSION_URL});
+    defer res.deinit(allocator);
 
     const newest_raw = std.mem.trim(u8, res.stdout, " \r\n");
 
@@ -56,12 +57,11 @@ pub fn run(context: *ctx.Context) !void {
     std.log.info("Updating dotman to v{d}.{d}.{d}...", .{ newest_major, newest_minor, newest_patch });
 
     // Run the install script to update
-    res.deinit(allocator);
-    res = try system.runCommand(allocator,
+    var res1 = try system.runCommand(allocator,
         "bash <(curl -s https://raw.githubusercontent.com/KDesp73/dotman/main/scripts/install.sh)",
         .{}
     );
-    defer res.deinit(allocator);
+    defer res1.deinit(allocator);
 
     std.log.info("Update complete!", .{});
 }
